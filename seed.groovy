4de17acd4b960453b371ce77ec71f001e6c3afd3
PradeepreddyKapa <pradeepreddy@zelarsoft.com>
2021-05-31 15:24:12 +0530

    ss

--- seed.groovy
+++ seed.groovy
@@ -39,3 +39,45 @@
       }
     }
   }
+
+  pipelineJob("user-ci") {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/PradeepreddyKapa/user.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
+
+  pipelineJob("todo-ci") {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/PradeepreddyKapa/todo.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
