e708e2a7aba23b871698462fa24b13fc6c834f4c
PradeepreddyKapa <pradeepreddy@zelarsoft.com>
2021-05-31 14:51:53 +0530

    ss

--- seed.groovy
+++ seed.groovy
@@ -18,3 +18,24 @@
       }
     }
   }
+
+  pipelineJob("login-ci") {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/PradeepreddyKapa/login.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
