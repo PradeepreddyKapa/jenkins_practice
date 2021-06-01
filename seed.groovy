3b675cbe628feebae03db46696ddd19f85cb3255
PradeepreddyKapa <pradeepreddy@zelarsoft.com>
2021-06-01 16:40:34 +0530

    ss

--- seed.groovy
+++ seed.groovy
@@ -15,7 +15,7 @@ for (i in 0..count) {
                     'userRemoteConfigs' {
                         'hudson.plugins.git.UserRemoteConfig' {
                             'url'('https://github.com/PradeepreddyKapa/'+j+'.git')
-                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/main/*\'')
                         }
                     }
                     'branches' {
