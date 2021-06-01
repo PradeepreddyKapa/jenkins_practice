d47b0666b84460e9f742783a130f42b55163589d
PradeepreddyKapa <pradeepreddy@zelarsoft.com>
2021-06-01 11:53:19 +0530

    ss

--- seed.groovy
+++ seed.groovy
@@ -1,35 +1,33 @@
-folder('CI-Pipelines'){
-  displayName('CI-Pipeline')
-  description('CI-Pipeline')
+folder('CI-Pipelines') {
+    displayName('CI Pipelines')
+    description('CI Pipelines')
 }
 
-def component = ["frontend", "login", "users", "todo"]
+def component = ["frontend", "login","users","todo"]
 
 def count=(component.size()-1)
 for (i in 0..count) {
-    def j = component[i]
-    pipelinejob("CI-Pipelines/${j}-ci") {
+    def j=component[i]
+    pipelineJob("CI-Pipelines/${j}-ci") {
         configure { flowdefinition ->
-            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
-                'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+                'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                     'userRemoteConfigs' {
                         'hudson.plugins.git.UserRemoteConfig' {
-                            'url'('https://github.com/PradeepreddyKapa/' + j + '.git')
+                            'url'('https://github.com/PradeepreddyKapa/'+j+'.git')
                             'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
-
                         }
                     }
                     'branches' {
                         'hudson.plugins.git.BranchSpec' {
-                            'name'('*/main')
+                            'name'('*/tags/*')
                         }
                     }
                 }
-                'scriptPath'('jenkinsfile')
+                'scriptPath'('Jenkinsfile')
                 'lightweight'(true)
             }
         }
     }
-
 }
 
