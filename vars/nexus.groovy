fb68e0130a3584adaf1eb5a1dc25f398c72a59de
PradeepreddyKapa <pradeepreddy@zelarsoft.com>
2021-06-12 11:36:18 +0530

    ss

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,9 +1,9 @@
-def nexus () {
+def nexus (COMPONENT) {
     get branch ="env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
     def get_branch_exec=sh(returnStdout: true, script: get_branch)
     def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
 
-    command = "curl -v -u admin:admin --upload-file ${FILENAME} http://172.31.12.138:8081/repository/${COMPONENT}/${FILENAME}"
+    command = "curl -v -u admin:admin --upload-file ${FILENAME} http://${NEXUS_IP}:8081/repository/${COMPONENT}/${FILENAME}"
     def execute_state=sh(returnStdout: true, script: command)
 }
 
