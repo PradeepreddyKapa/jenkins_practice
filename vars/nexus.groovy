e6a5d5a81a61f1eabeb60ae29a0fb66f4c0ff17c
PradeepreddyKapa <pradeepreddy@zelarsoft.com>
2021-06-12 12:26:48 +0530

    ss

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,5 +1,5 @@
 def nexus (COMPONENT) {
-    get branch ="env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
+    get_branch ="env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
     def get_branch_exec=sh(returnStdout: true, script: get_branch)
     def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
 
@@ -8,7 +8,7 @@ def nexus (COMPONENT) {
 }
 
 def make_artifacts(APP_TYPE, COMPONENT) {
-    get branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
+    get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
     def get_branch_exec=sh(returnStdout: true, script: get_branch)
     def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
     if(APP_TYPE == "NGINX" ) {
