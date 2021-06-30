2e2a2e5be5e3f9af48d4e095e04089fd5b5250da
PradeepreddyKapa <82633584+PradeepreddyKapa@users.noreply.github.com>
2021-06-30 11:45:47 +0530

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -32,7 +32,11 @@ def make_artifacts(APP_TYPE, COMPONENT) {
 }
 
 def code_build(APP_TYPE, COMPONENT) {
-    if(APP_TYPE == "MAVEN"){
+    if(APP_TYPE == "NGINX"){
+        command = "npm install && npm run build"
+        def execute_com=sh(returnStdout: true, script: command)
+        print execute_com
+    }   else if(APP_TYPE == "MAVEN"){
         command = " mvn clean package "
         def execute_com=sh(returnStdout: true, script: command )
         print execute_com
